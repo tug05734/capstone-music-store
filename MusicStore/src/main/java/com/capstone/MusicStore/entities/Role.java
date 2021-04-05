0b43a6bd02af41f4c5a6648cf42b92d7a55b4e30
John Galarza-Calderon <john.galarzacalderon@hcl.com>
2021-04-05 14:41:07 -0400

    Added the backbone for spring security. Setup a config package, controllers package, entities package, repositories package, and services package as well as basic placeholder html pages utilizing bootstrap and thymeleaf. Be sure to edit your application.properties file to connect to your mysql instance correctly.

---
+++ MusicStore/src/main/java/com/capstone/MusicStore/entities/Role.java
@@ -0,0 +1,44 @@
+package com.capstone.MusicStore.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "role_id")
+	private int id;
+	
+	@Column(name = "role")
+	private String role;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	@Override
+	public String toString() {
+		return "Role [id=" + id + ", role=" + role + "]";
+	}
+	
+	
+}

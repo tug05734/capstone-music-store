317541874e40708b0d2471a25760ea164cf3dbc6
Rajat Rajan <tug05734@temple.edu>
2021-04-05 10:28:50 -0400

    Added Spring project

---
+++ MusicStore/src/test/java/com/capstone/MusicStore/MusicStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.MusicStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

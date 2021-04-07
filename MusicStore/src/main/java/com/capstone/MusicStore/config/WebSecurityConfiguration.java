8312a99db8173923adda23ca006f9cdbeb1e52c2
Rajat Rajan <tug05734@temple.edu>
2021-04-07 19:59:54 -0400

    Adding views for product catalog and product details, added string field for image path in product entity, implemented get method to show product details

--- MusicStore/src/main/java/com/capstone/MusicStore/config/WebSecurityConfiguration.java
+++ MusicStore/src/main/java/com/capstone/MusicStore/config/WebSecurityConfiguration.java
@@ -3,10 +3,12 @@ package com.capstone.MusicStore.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -41,6 +43,9 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/").permitAll()
                 .antMatchers("/login").permitAll()
                 .antMatchers("/registration").permitAll()
+                .antMatchers("/products/all").permitAll() //for testing
+                .antMatchers("/products/search").permitAll()
+                .antMatchers("/products/details/{id}").permitAll()
                 .antMatchers("/user/**").hasAnyAuthority("ADMIN", "USER").anyRequest()
                 .authenticated().and().csrf().disable().formLogin()
                 .loginPage("/login").failureUrl("/login?error=true")
@@ -57,7 +62,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     public void configure(WebSecurity web) throws Exception {
         web
                 .ignoring()
-                .antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**");
+                .antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**", "/styles/**");
     }
 
 }
